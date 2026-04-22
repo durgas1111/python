fd1d5e2fd44a022e3779cd88f4009bd93eb9d9a3
Durga Alagarsamy <durgas1111@gmail.com>
2026-04-21 20:35:17 -0400

    condifelse.py

---
+++ condifelse.py
@@ -0,0 +1,16 @@
+age = 18
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible")
+
+Console
+Run
+Run started
+Initializing environment
+Installing packages
+Running code
+Eligible to vote
+
+Run completed in 1062ms
