b4e85c94a5425d641a4ce8508439bfb40d704396
Durga Alagarsamy <durgas1111@gmail.com>
2026-04-21 20:12:25 -0400

    var

---
+++ var.py
@@ -0,0 +1,19 @@
+name = "Durga"
+age = 30
+salary = 75000.50
+is_active = True
+
+print(name, age, salary, is_active)
+
+
+Output:
+
+Console
+Run
+Run started
+Initializing environment
+Installing packages
+Running code
+Durga 30 75000.5 True
+
+Run completed in 1348ms
