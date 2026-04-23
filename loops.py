cde6441a191458dc276028ff52275988d3d2e1c5
Durga Alagarsamy <durgas1111@gmail.com>
2026-04-22 22:04:58 -0400

    loops.py

---
+++ loops.py
@@ -0,0 +1,45 @@
+🟢 3. Loops
+✅ For Loop
+for i in range(5):
+    print(i)
+✅ While Loop
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
+Console
+Run
+Run started
+Initializing environment
+Installing packages
+Running code
+0
+
+1
+
+2
+
+3
+
+4
+
+Run completed in 1157ms
+
+Console
+Run
+Run started
+Initializing environment
+Installing packages
+Running code
+0
+
+1
+
+2
+
+3
+
+4
+
+Run completed in 1052ms
