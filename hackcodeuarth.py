bcd71efef51d5782cd79221cdb92459ebf367eb4
Durga Alagarsamy <durgas1111@gmail.com>
2026-04-23 21:01:42 -0400

    hackcodeuarth.py

---
+++ hackcodeuarth.py
@@ -0,0 +1,82 @@
+Task
+The provided code stub reads two integers from STDIN, and . Add code to print three lines where:
+
+    The first line contains the sum of the two numbers.
+    The second line contains the difference of the two numbers (first - second).
+    The third line contains the product of the two numbers.
+
+Example
+
+Print the following:
+
+8
+-2
+15
+
+Input Format
+
+The first line contains the first integer, .
+The second line contains the second integer, .
+
+Constraints
+
+
+Output Format
+
+Print the three lines as explained above.
+
+Sample Input 0
+
+3
+2
+
+Sample Output 0
+
+5
+1
+6
+
+Explanation 0
+
+
+3+2=5
+3-2=1
+3*2=6
+
+
+
+Output:
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+ 
+    
+print(a + b)
+print(a - b)
+print(a * b) 
+
+Congratulations!
+
+You have passed the sample test cases. Click the submit button to run your code against all the test cases.
+Input (stdin)
+
+    3
+
+    2
+
+Your Output (stdout)
+
+    5
+
+    1
+
+    6
+
+Expected Output
+
+    5
+
+    1
+
+    6
